0b94853d3815f487edc8f69c4d48690d24ea9123
hiendnguyen <nguyenduyhien@gmail.com>
2021-05-28 22:09:38 -0700

    remove secret key

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-7(o64whin*@p4e3tshyn^!q*j7v@ssu%r-&5!7mvi6fx*z0=vb'
 from django.core.management.utils import get_random_secret_key
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
